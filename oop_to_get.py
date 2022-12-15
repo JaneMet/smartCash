56df8a0084df1798d44212635f8df8b5a1ee728c
JaneMet <metje@mail.ru>
2022-12-15 14:46:06 +0300

    perfect

--- oop_to_get.py
+++ oop_to_get.py
@@ -71,7 +71,7 @@ class CashCalculator(Calculator):
 
         com_bad = 'Денег нет, держись'
         com_better = 'Денег нет, держись: твой долг - N руб/USD/Euro {cash_remained}{money}'
-        com_ost = f'Сегодня осталось {cash_remained}{money}'
+        com_ost = f'Сегодня осталось {cash_remained} {money}'
         com_not_in = 'Валюта {money} не рассматривается в программе'
 
         if cash_remained == 0:
@@ -100,9 +100,9 @@ cash_calculator.add_record(Record(amount=3000,
                                   comment='бар в Танин др',
                                   date='08.11.2019'))
 
-print(f'week stst {cash_calculator.get_week_stats()}')
-print(f'cash rem for td {cash_calculator.get_today_cash_remained()}')
-print(f'limit blnc for today {cash_calculator.get_today_limit_balance()}')
+#print(f'1. week stst {cash_calculator.get_week_stats()}') 1. week stst 445
+print(f'2. {cash_calculator.get_today_cash_remained()}')
+#print(f'3. limit blnc for today {cash_calculator.get_today_limit_balance()}') 3. limit blnc for today 555
 
 calories_calculator = CaloriesCalculator(3000)
 calories_calculator.add_record(Record(amount=1186,comment='Кусок тортика. И ещё один.', date='24.02.2019'))
@@ -110,6 +110,6 @@ calories_calculator.add_record(Record(amount=84, comment='Йогурт.', date='
 calories_calculator.add_record(Record(amount=1140, comment='Баночка чипсов.', date='24.02.2019'))
 
 
-print(f' calories rem for today {calories_calculator.get_calories_remained()}')
+#print(f'5.{calories_calculator.get_calories_remained()}') 5.Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более 3000 кКал
 # должно напечататься
 # На сегодня осталось 555 руб
